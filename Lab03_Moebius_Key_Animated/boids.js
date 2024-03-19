2776da9a93724eb970e4bae207730ba4da051b92
UnknownPack <batoonnestor@gmail.com>
2024-03-19 19:38:05 +1100

    First version of completed boid class

--- Lab03_Moebius_Key_Animated/boids.js
+++ Lab03_Moebius_Key_Animated/boids.js
@@ -1,8 +1,9 @@
 import * as THREE from 'three';
+export default Boid;
 
 class Boid{
 
-    constructor(position, velocity, maxSpeed, maxForce, searchRadius, lightPoint, lightAttraction) {
+    constructor(position, velocity, maxSpeed, maxForce, searchRadius, lightPoint, lightAttraction, scene) {
         this.position = position; 
         this.velocity = velocity; 
         this.maxSpeed = maxSpeed; 
@@ -10,23 +11,62 @@ class Boid{
         this.lightPoint = lightPoint;  
         this.lightAttraction = lightAttraction;
         this.searchRadius = searchRadius; 
+        this.scene = scene;
+        this.boidMesh = null; 
+
+        this.initBoidMesh();
     }
 
     update(){
-        
+        this.velocity.clampLength(0, this.maxSpeed);
+        this.position.add(this.velocity);
+
+        if (this.boidMesh) {
+            //sets boid mesh position
+            this.boidMesh.position.copy(this.position);
+        }
     }
 
     render(){
-        
+        //checks if boidMesh is not null and if this mesh is not already in the scene
+        if (this.boidMesh && !this.scene.getObjectById(this.boidMesh.id)) {
+            //if both are true, it adds the boid mesh to the scene
+            this.scene.add(this.boidMesh);
+        }
+    }
+
+    initBoidMesh() {
+        // I'd reckon you can change the mesh of the moth here
+        // I will use spheres to represent the moth
+
+        const geometry = new THREE.SphereGeometry(1, 16, 16);
+        const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+        this.boidMesh = new THREE.Mesh(geometry, material);
+        this.boidMesh.position.copy(this.position);
     }
 
 
     attractionToLight(){
-
+        const lightAttractionForce = new THREE.Vector3().subVectors(this.lightPoint, this.position);
+        lightAttractionForce.multiplyScalar(this.lightAttraction); 
+        return lightAttractionForce;
     }
 
-    avoidanceBehaviour(){
-        let avoidanceForce = new THREE.Vector3();
+    avoidanceBehaviour(obstacles){
+        let avoidanceForce = new THREE.Vector3(); 
+    
+        for (let obstacle of obstacles) {  
+            if (obstacle !== this) { 
+                var distance = this.position.distanceTo(obstacle.position);  
+                if (distance < this.searchRadius) {  
+                    var direction = new THREE.Vector3().subVectors(this.position, obstacle.position).normalize(); 
+                    avoidanceForce.add(direction);  
+                }
+            }
+        }
+    
+        return avoidanceForce;  
     }
+    
 
 }
